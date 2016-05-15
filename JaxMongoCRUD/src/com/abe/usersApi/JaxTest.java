2849b7ff876bfc8bfecf90e23c386d0b45f46ed9
abe-Desktop <abe.boujane@gmail.com>
2016-05-15 14:47:50 -0400

    Adding a method to GET a single user

--- JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
+++ JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
@@ -2,6 +2,7 @@ package com.abe.usersApi;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -10,15 +11,21 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
+/**
+ * @author abe
+ *This Class Follows the REST architecture/naming convention 
+ *to interact with a DB of users
+ */
 @Path("/users")
 public class JaxTest {
 
+//	This method returns all users in the DB
 	@GET
 	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
 	public List<DBUser> getUsers(){
 		return DBConnection.getList();
 	}
-	
+//	This method allows to add a new user to the DB
 	@POST
 	@Consumes({"application/json","application/xml"})
 	@Produces(MediaType.TEXT_PLAIN)
@@ -26,5 +33,11 @@ public class JaxTest {
 		return DBConnection.addUser(user);
 	}
 	
+	@GET
+	@Path("{emailAddress}")
+	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+	public DBUser getUser(@PathParam("emailAddress") String emailAddress){
+		return DBConnection.getUser(emailAddress);
+	}
 	
 }
