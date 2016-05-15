130a73e54de6dbbf9860d4409b33079c50d24ff9
abe-Desktop <abe.boujane@gmail.com>
2016-05-15 17:27:53 -0400

    Implemented the PUT method to modify existing users

--- JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
+++ JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
@@ -6,15 +6,15 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * @author abe 
- * This Class Follows the REST architecture/naming convention to
- *  interact with a DB of users
+ * @author Abe Boujane This Class Follows the REST architecture/naming
+ *         convention to interact with a DB of users
  */
 @Path("/users")
 public class JaxTest {
@@ -42,8 +42,7 @@ public class JaxTest {
 	public String addUser(DBUser user) {
 		return DBConnection.addUser(user);
 	}
-	
-	
+
 	@DELETE
 	@Path("{emailAddress}")
 	@Produces({ MediaType.TEXT_PLAIN })
@@ -51,4 +50,10 @@ public class JaxTest {
 		return DBConnection.deleteUser(emailAddress);
 	}
 
+	@PUT
+	@Path("{emailAddress}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public DBUser modifyUser(@PathParam("emailAddress") String oldEmailAddress, DBUser user) {
+		return DBConnection.modifyUser(oldEmailAddress, user);
+	}
 }
