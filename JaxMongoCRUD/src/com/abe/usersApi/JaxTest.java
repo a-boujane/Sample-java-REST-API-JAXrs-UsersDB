f3f753dea2a203ae92564f8f8057d6692feaaf8b
abe-Desktop <abe.boujane@gmail.com>
2016-05-15 00:58:38 -0400

    Adding 'POST' support, allowing for new users to be added to the DB
    this supports both (xml/json)

--- JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
+++ JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
@@ -2,7 +2,9 @@ package com.abe.usersApi;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,8 +15,17 @@ public class JaxTest {
 
 	@GET
 	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-	public List<DBUser> itWorks(){
+	public List<DBUser> getUsers(){
 		return DBConnection.getList();
 	}
+	
+	@POST
+	@Consumes({"application/json","application/xml"})
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addUser(DBUser user){
+		DBConnection.addUser(user);
+		return "Merci!";
+	}
+	
 	
 }
