84e347314d78b0e2c4fa52feca84bce2c48143c0
abe-Desktop <abe.boujane@gmail.com>
2016-05-15 00:17:30 -0400

    Cleaning up the JaxTest - currently /users for json and XML

--- JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
+++ JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
@@ -6,8 +6,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.NewCookie;
-import javax.ws.rs.core.Response;
 
 
 @Path("/users")
@@ -17,14 +15,6 @@ public class JaxTest {
 	@Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
 	public List<DBUser> itWorks(){
 		return DBConnection.getList();
-	}
-	
-	@GET
-	@Path("/cooki")
-	@Produces(MediaType.TEXT_PLAIN)
-	public Response echoRequest(){
-		
-		return Response.ok().cookie(new NewCookie("cookieName","cookievalue")).build();
 	}
 	
 }
