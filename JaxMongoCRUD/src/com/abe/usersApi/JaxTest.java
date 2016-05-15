58087842d97c4956f2d717148ef8f18a1e4e86f2
abe-Desktop <abe.boujane@gmail.com>
2016-05-14 22:12:41 -0400

    First Commit

---
+++ JaxMongoCRUD/src/com/abe/usersApi/JaxTest.java
@@ -0,0 +1,29 @@
+package com.abe.usersApi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+
+@Path("/users")
+public class JaxTest {
+
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public User itWorks(@Context HttpHeaders hh ){
+//		User u = new User("Abe","Boujane","Abeb_admin",24);
+//		return u;
+//	}
+	
+//	@GET
+//	@Path("/cooki")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public Response echoRequest(){
+//		
+//		return Response.ok().cookie(new NewCookie("cookieName","cookievalue")).build();
+//	}
+	
+}
---
+++ JaxMongoCRUD/src/com/abe/usersApi/User.java
@@ -0,0 +1,83 @@
+package com.abe.usersApi;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class User {
+
+	@XmlElement(required = true)
+	protected String fname;
+
+	@XmlElement(required = true)
+	protected String lname;
+
+	@XmlElement(required = true)
+	protected String uname;
+
+	@XmlElement(required = true)
+	protected int age;
+
+	@XmlElement(required = true)
+	protected String email;
+
+	@XmlElement(required = true)
+	protected String pic;
+
+	public User() {
+
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPic() {
+		return pic;
+	}
+
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+
+}
